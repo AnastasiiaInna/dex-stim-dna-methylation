c897fda0664468c01e97f0c13c36765da4847cb3
anastasiiainna <anastasiia_hry@PCR183.local>
2021-05-26 09:58:10 +0200

    sva computation on M-values

--- 06_sva/01_sva.R
+++ 06_sva/01_sva.R
@@ -2,7 +2,10 @@
 
 # Reference: https://bioconductor.org/packages/release/bioc/vignettes/sva/inst/doc/sva.pdf
 
+setwd("~/bio/code/mpip/dex-stim-human-dna-methyl-qc/")
+
 library(sva)
+library(lumi) # convert beta values to M-values 
 
 input.parameters.fn <- "input_parameters.csv"
 input.parameters    <- read.csv(input.parameters.fn, sep = ";", header = F )
@@ -17,15 +20,26 @@ pheno <- get(x)
 
 x        <- load(beta.combat.expr.set.fn) #Betas_combated_ExprSet
 expr.set <- get(x)
-
 rm(x)
 
-# 2. Create the full and null model matrices
-mod  <- model.matrix(~Sample_Group, data = pheno)
-mod0 <- model.matrix(~ 1, data = pheno))
+# beta.mtrx.fn <- "~/bio/datasets/methylation/10_final_qc_data/dex_methyl_beta_combat_mtrx.rds"
+# beta.mtrx    <- readRDS(beta.mtrx.fn)
+# pheno  <- pheno[pheno$Sample_Name %in% colnames(beta.mtrx),]
 
-# 3. Applying the 'sva' to estimate surrogate variables
-n.sv  <- num.sv(expr.set, mod, method="leek")
-svobj <- sva(expr.set, mod, mod0, n.sv = n.sv)
+# pheno <- fread("~/bio/datasets/pheno/pheno_full_for_kimono.csv", na.strings = "#N/A")
+# pheno <- pheno[!is.na(DNAm_ID)]
+
+#2. Convert beta values to M-values
+
+m.mtrx <- beta2m(beta.mtrx)
+
+# 3. Create the full and null model matrices
+mod  <- model.matrix(~ Sample_Group, data = pheno)
+mod0 <- model.matrix(~ 1, data = pheno)
+
+# 4. Applying the 'sva' to estimate surrogate variables
+n.sv  <- num.sv(m.mtrx, mod, method="leek")
+svobj <- sva(m.mtrx, mod, mod0, n.sv = n.sv)
+
 
 
